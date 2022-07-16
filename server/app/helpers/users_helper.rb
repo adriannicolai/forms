2fd9f431dbad234a87524c36075f1edcd338fc70
adriannicolai <anlumbad@gmail.com>
2022-07-16 17:41:40 +0800

    added validation of new user

---
+++ server/app/helpers/users_helper.rb
@@ -0,0 +1,45 @@
+module UsersHelper
+    # DOCU: Validate new user information
+    # Triggered by: multiple models
+    # Requires: params
+    # Returns: {status, result{params}, error}
+    # Last updated at: July 16, 2022
+    # Owner: Adrian
+    def validate_new_user_info(params)
+        response_data = { :status => false, :result => {}, :error => nil }
+
+        begin
+            validations = {
+                :name     => { :regex => /[@%^&!"\\\*\.,\-\:?\/\'=`{}()+_\]\|\[\><~;$#0-9]/, :error => "Special Characters are not allowed on name" },
+                :password => { :regex => /^(?=.*?[A-Z])(?=.*?[0-9]).{0,}$/, :error => "Password must have an uppercase letter and a number" },
+                :email    => { :regex => URI::MailTo::EMAIL_REGEXP, :error => "Please enter a valid email" }
+            }
+
+            params.each do |key, value|
+                validation_key  = [:first_name, :last_name].include?(key) ? :name : key
+
+                if validation_key === :password && params[:confirm_password].present?
+                    response_data[:result][:password] = "Passwords do not match" if params[:password] != params[:confirm_password]
+                end
+
+                if validation_key === :email
+                    response_data[:result][:email] = "User already Exists" if User.get_user_record({ :fields_to_filter => { :email => value } })[:status]
+                    # Downcase the email
+                    value = value.downcase
+                end
+
+                next if validations[validation_key].nil?
+
+                validation_comparison = validations[validation_key][:regex] =~ value
+
+                response_data[:result][validation_key] = validations[validation_key][:error] if validation_key === :name ? !validation_comparison.nil? : validation_comparison.nil?
+            end
+
+            response_data[:status] = !response_data[:result].present?
+        rescue Exception => ex
+            response_data[:error] = ex.message
+        end
+
+        return response_data
+    end
+end
