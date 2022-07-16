9df1b49dc5573e53c20aa71bf90f108377589794
adriannicolai <anlumbad@gmail.com>
2022-07-16 18:53:17 +0800

    added create user

---
+++ server/app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+	# DOCU: Process adding of company and role, and adding/updating company_cand_preferences record
+	# Triggered by (POST) /user/create_user
+	# Requires: params - first_name, last_name, email, password, confirm_password
+	# Returns: { status: true/false, result: { user_details }, error }
+	# Last udpated at: July 11, 2022
+	# Owner: Adrian
+	def create_user
+		response_data = { :status => false, :result => {}, :error => nil }
+
+		begin
+			create_new_user = User.create_user(params)
+		rescue Exception => ex
+			response_data[:error] = ex.message
+		end
+
+		return response_data
+	end
+
+	def update_user
+	end
+end
