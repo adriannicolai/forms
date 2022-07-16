28577d729f765145da2924513c0906500864f41b
adriannicolai <anlumbad@gmail.com>
2022-07-16 17:36:37 +0800

    initial commit for add user route

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+	# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+	scope "/user" do
+		post "/create_user" => "users#create_user"
+	end
+	# Defines the root path route ("/")
+	root "forms#landing_page"
 end
