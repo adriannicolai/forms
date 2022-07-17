ee79d4b824f9d54b0501eb3fecc4523074ae7d97
adriannicolai <anlumbad@gmail.com>
2022-07-17 19:14:19 +0800

    added method fgor logging in user in model and fixed get_user_record

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -46,13 +46,43 @@ class User < ApplicationRecord
         return response_data
     end
 
+    # DOCU: Method for processing the login for users
+    # Triggered by UsersController#create_user
+	# Requires: params - first_name, last_name, email, password, confirm_password
+    # Returns: { status: true/false, result: { user_details }, error }
+    # Last updated at: July 16, 2022
+    # Owner: Adrian
+    def self.login_user(params)
+        response_data = { :status => false, :result => {}, :error => nil }
+
+        begin
+            #Validate user parameters
+            check_user_params = check_fields(["email", "password"], [], params)
+
+            if check_user_params[:status]
+                # Find the user record
+                user_details = self.get_user_record({ :fields_to_filter => { :email => check_user_params[:result][:email].downcase, :password => encrypt_password(check_user_params[:result][:password]) }})
+
+                # Add the user details to be returned
+                response_data.merge!(user_details)
+            else
+                response_data.merge!(check_user_params)
+            end
+
+        rescue Exception => ex
+            response_data[:error] = ex.message
+        end
+
+        return response_data
+    end
+
     private
         # DOCU: Method to insert candidate for newly created users invited for interview
         # Triggered by UsersController#create_user
-        # Requires:  params - fields_to_select
-        # Optionals: params - fields_to_filter
+        # Requires:  params - fields_to_filter
+        # Optionals: params - fields_to_select
         # Returns: { status: true/false, result: { user_details }, error }
-        # Last updated at: July 16, 2022
+        # Last updated at: July 17, 2022
         # Owner: Adrian
         def self.get_user_record(params)
             response_data = { :status => false, :result => {}, :error => nil }
@@ -66,7 +96,7 @@ class User < ApplicationRecord
                 # Add to the where clause
                 if params[:fields_to_filter].present?
                     params[:fields_to_filter].each_with_index do |(field, value), index|
-                        select_user_query[0] += "#{' AND' if index > 1} #{field} #{field.is_a?(Array) ? 'IN(?)' : '= ?'}"
+                        select_user_query[0] += "#{' AND' if index > 0} #{field} #{field.is_a?(Array) ? 'IN(?)' : '= ?'}"
                         select_user_query << value
                     end
                 end
