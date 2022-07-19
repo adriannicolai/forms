858bcce7b049f4c8c0c77fbb660b379878e5121f
adriannicolai <anlumbad@gmail.com>
2022-07-19 21:30:45 +0800

    added default constant

---
+++ server/config/initializers/default_field_constants.rb
@@ -0,0 +1,10 @@
+DEFAULT_FORM_SETTING = '{
+    "respondent_settings": {
+        "is_editable": 1,
+        "is_correct_answer_seen": 1
+        },
+    "quiz_mode_settings":{
+            "is_quiz_mode": 0,
+            "default_point_value": 1
+        }
+}'
