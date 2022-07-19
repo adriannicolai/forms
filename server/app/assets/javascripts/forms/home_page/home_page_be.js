4754b356bab1937a516eb63eab99300dd06f1082
adriannicolai <anlumbad@gmail.com>
2022-07-19 21:29:52 +0800

    initial commit for js files

--- server/app/assets/javascripts/forms/home_page/home_page_be.js
+++ server/app/assets/javascripts/forms/home_page/home_page_be.js
@@ -1,15 +1,28 @@
 $(document).ready(function(){
     $(document)
-        .on("change", "#form_sort_order", updateFormSortOrder)
+        .on("submit", "#get_all_forms", submitGetAllForms)      /* This function will submit the get all forms and update the forms list */
+        .on("change", "#form_sort_order", updateFormSortOrder)   /* This function will handle the submission of the form sort order  */
+        .on("click", "#add_form_button", submitCreateNewForm);   /* This function will trigger the submission of the create new form */
 });
 
+/**
+* DOCU: This function will change the sort order of fetching forms and submit the get all form<br>
+* Triggered: .on("change", "#form_sort_order", updateFormSortOrder)<br>
+* Last Updated Date: July 19, 2022
+* @author Adrian
+*/
 function updateFormSortOrder(){
     let GetAllForms = $("#get_all_forms");
 
     /* TODO: will update this */
 }
 
-
+/**
+* DOCU: This function will handle the submission the get all forms and update the forms list<br>
+* Triggered: .on("submit", "#get_all_forms", submitGetAllForms)<br>
+* Last Updated Date: July 19, 2022
+* @author Adrian
+*/
 function submitGetAllForms(){
     let GetAllForms = $("#get_all_forms");
 
@@ -18,4 +31,24 @@ function submitGetAllForms(){
 
         /* TODO: submit form here */
     }
+}
+
+/**
+* DOCU: This function will trigger the submission of create new form<br>
+* Triggered: .on("click", "#add_form_button", submitCreateNewForm)<br>
+* Last Updated Date: July 19, 2022
+* @author Adrian
+*/
+function submitCreateNewForm(){
+    let createForm = $("#create_form");
+
+    $.post(createForm.attr("action"), createForm.serialize(), function(create_form_response){
+        console.log(create_form_response);
+        if(create_form_response.status){
+
+        }
+        else{
+            alert(create_form_response.error);
+        }
+    });
 }
