005c91c54e56870f79d86543066f0f6152755405
adriannicolai <anlumbad@gmail.com>
2022-07-20 07:16:06 +0800

    added get all forms query

--- server/app/models/form.rb
+++ server/app/models/form.rb
@@ -22,7 +22,7 @@ class Form < ApplicationRecord
 
                 if create_form.present?
                     response_data[:status] = true
-                    response_data[:result] = self.find_form_record({:fields_to_filter => { :id => create_form }})
+                    response_data[:result] = self.get_form_record({:fields_to_filter => { :id => create_form }})
                 else
                     response_data[:error]  = "Error creating form record, Please try again later"
                 end
@@ -44,7 +44,7 @@ class Form < ApplicationRecord
         # Returns: { status: true/false, result: { form_details }, error }
         # Last updated at: July 20, 2022
         # Owner: Adrian
-        def self.find_form_record(params)
+        def self.get_form_record(params)
             response_data = { :status => false, :result => {}, :error => nil }
 
             begin
@@ -62,6 +62,54 @@ class Form < ApplicationRecord
                 form_details = query_record(select_form_query)
 
                 response_data.merge!(form_details.present? ? { :status => true, :result => form_details } : { :error => "Form not found" } )
+            rescue Exception => ex
+                response_data[:error] = ex.message
+            end
+
+            return response_data
+        end
+
+        # DOCU: Method to fetch form records
+        # Triggered by Forms
+        # Requires: params - fields_to_select, fields_to_filter
+        # Returns: { status: true/false, result: { form_details }, error }
+        # Last updated at: July 20, 2022
+        # Owner: Adrian
+        def self.get_form_records(params)
+            response_data = { :status => false, :result => {}, :error => nil }
+
+            begin
+                params[:fields_to_select] ||= "*"
+
+                select_form_query = ["SELECT #{ActiveRecord::Base.sanitize_sql(params[:fields_to_select])} FROM
+                    forms WHERE
+                "]
+
+                params[:fields_to_filter].each_with_index do |(field, value), index|
+                    select_form_query[0] += " #{ 'AND' if index > 0} #{field} = ?"
+                    select_form_query << value
+                end
+
+                form_details = query_records(select_form_query)
+
+                response_data.merge!(form_details.present? ? { :status => true, :result => form_details } : { :error => "Form not found" } )
+            rescue Exception => ex
+                response_data[:error] = ex.message
+            end
+
+            return response_data
+        end
+
+        # DOCU: Method to set query setings such as sort order, limit for pagination settings
+        # Triggered by Forms
+        # Returns: { status: true/false, result: { query_settings }, error }
+        # Last updated at: July 20, 2022
+        # Owner: Adrian
+        def self.format_get_form_query_settings
+            response_data = { :status => false, :result => {}, :error => nil }
+
+            begin
+
             rescue Exception => ex
                 response_data[:error] = ex.message
             end
