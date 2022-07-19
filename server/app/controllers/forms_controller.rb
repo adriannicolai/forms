45fe178293c70ec353fa0c885ce7e6afc4067dfd
adriannicolai <anlumbad@gmail.com>
2022-07-19 21:30:18 +0800

    added the creation of new form

--- server/app/controllers/forms_controller.rb
+++ server/app/controllers/forms_controller.rb
@@ -21,6 +21,26 @@ class FormsController < ApplicationController
 
 	end
 
+	# DOCU: This will handle the creation of new forms
+    # Triggered by: (POST) /forms/create_forms
+	# Session - user_id
+    # Returns: { status: true/false, result: { form_data }, error }
+    # Last updated at: July 19, 2022
+    # Owner: Adrian
+	def create_form
+		response_data = { :status => false, :result => {}, :error => nil }
+
+		begin
+			create_form = Form.create_form(params.merge!({ :user_id => session[:user_id] }))
+
+			response_data.merge!(create_form)
+		rescue Exception => ex
+			response_data[:error] = ex.message
+		end
+
+		render :json => response_data
+	end
+
 	private
 		# DOCU: Redirects the user to the landing page if there is no session
 		# Triggered by before_action
--- server/app/models/form.rb
+++ server/app/models/form.rb
@@ -1,3 +1,39 @@
+include ApplicationHelper
+include QueryHelper
+
 class Form < ApplicationRecord
+    # DOCU: Method to insert candidate for newly created users invited for interview
+    # Triggered by FormsController#create_form
+	# Requires: params - user_id
+    # Returns: { status: true/false, result: { form_details }, error }
+    # Last updated at: July 19, 2022
+    # Owner: Adrian
+    def self.create_form(params)
+        response_data = { :status => false, :result => {}, :error => nil }
+
+        begin
+            check_new_form_params = check_fields(["user_id"], [], params)
+
+            if check_new_form_params[:status]
+                create_form = insert_record(["
+                    INSERT INTO forms (user_id, form_settings_json, cache_response_count, created_at, updated_at)
+                    VALUES (?, ?, 0, NOW(), NOW())
+                ", check_new_form_params[:result][:user_id], DEFAULT_FORM_SETTING])
+
+                if create_form.present?
+
+                else
+
+                end
+            else
+                response_data.merge!(check_new_form_params)
+            end
+
+        rescue Exception => ex
+            response_data[:error] = ex.message
+        end
+
+        return response_data
+    end
 
 end
