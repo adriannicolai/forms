fc5a04d767a4393de02fb36771aca549ce9f4915
adriannicolai <anlumbad@gmail.com>
2022-07-20 19:56:03 +0800

    fixed typo

--- server/db/migrate/20220717115640_create_form_section.rb
+++ server/db/migrate/20220717115640_create_form_section.rb
@@ -2,7 +2,7 @@ class CreateFormSection < ActiveRecord::Migration[7.0]
   def change
     create_table :form_sections do |t|
       t.references :form, null: false, foreign_key: true
-      t.text :forn_question_ids
+      t.text :form_question_ids
 
       t.timestamps
     end
