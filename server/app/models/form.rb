124b4ac719d1a098a97c5a3dbea51bd2f61d6826
adriannicolai <anlumbad@gmail.com>
2022-07-26 21:22:25 +0800

    moved to proper controllers

--- server/app/models/form.rb
+++ server/app/models/form.rb
@@ -26,7 +26,7 @@ class Form < ApplicationRecord
                     if created_form_id.present?
                         response_data[:status]         = true
                         # Create a new template section upon creating a new form
-                        create_template_section_record = self.create_form_section_record({ :form_id => created_form_id })
+                        create_template_section_record = FormSection.create_form_section_record({ :form_id => created_form_id })
                         response_data[:result][:id]    = encrypt(created_form_id)
 
                         raise "Error in creating template section record, Please try again later" if !create_template_section_record[:status]
@@ -38,44 +38,6 @@ class Form < ApplicationRecord
                 response_data.merge!(check_new_form_params)
             end
 
-        rescue Exception => ex
-            response_data[:error] = ex.message
-        end
-
-        return response_data
-    end
-
-    # DOCU: Method to insert form section record
-    # Triggered by FormsController#create_form
-	# Requires: params - user_id
-    # Returns: { status: true/false, error }
-    # Last updated at: July 24, 2022
-    # Owner: Adrian
-    def self.create_form_section_record(params)
-        response_data = { :status => false, :result => {}, :error => nil }
-
-        begin
-            create_form_section_id = insert_record(["
-                INSERT INTO form_sections (form_id, form_question_ids, created_at, updated_at)
-                VALUES (?, '[]', NOW(), NOW())", params[:form_id]
-            ])
-
-            # Create a new form question after creating a section
-            if create_form_section_id.present?
-                create_form_question = FormQuestion.create_form_question({
-                    "question_type_id" => QUESTION_SETTINGS[:question_type][:paragraph],
-                    "form_id"          => params[:form_id],
-                    "form_section_id"  => create_form_section_id
-                })
-
-                if create_form_question[:status]
-                    response_data.merge!(create_form_question)
-                else
-                    raise create_form_question[:error]
-                end
-            else
-                raise "Somethign went wrong in creatting form section, Please try again later"
-            end
         rescue Exception => ex
             response_data[:error] = ex.message
         end
--- server/app/models/form_section.rb
+++ server/app/models/form_section.rb
@@ -1,2 +1,42 @@
+include ApplicationHelper
+include QueryHelper
+
 class FormSection < ApplicationRecord
+    # DOCU: Method to insert form section record
+    # Triggered by FormsController#create_form
+	# Requires: params - user_id
+    # Returns: { status: true/false, error }
+    # Last updated at: July 27, 2022
+    # Owner: Adrian
+    def self.create_form_section_record(params)
+        response_data = { :status => false, :result => {}, :error => nil }
+
+        begin
+            create_form_section_id = insert_record(["
+                INSERT INTO form_sections (form_id, form_question_ids, created_at, updated_at)
+                VALUES (?, '[]', NOW(), NOW())", params[:form_id]
+            ])
+
+            # Create a new form question after creating a section
+            if create_form_section_id.present?
+                create_form_question = FormQuestion.create_form_question({
+                    "question_type_id" => QUESTION_SETTINGS[:question_type][:paragraph],
+                    "form_id"          => params[:form_id],
+                    "form_section_id"  => create_form_section_id
+                })
+
+                if create_form_question[:status]
+                    response_data.merge!(create_form_question)
+                else
+                    raise create_form_question[:error]
+                end
+            else
+                raise "Somethign went wrong in creatting form section, Please try again later"
+            end
+        rescue Exception => ex
+            response_data[:error] = ex.message
+        end
+
+        return response_data
+    end
 end
