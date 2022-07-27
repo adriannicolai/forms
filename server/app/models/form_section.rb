1beb731982c15a5ba707a019b08b745771c49050
adriannicolai <anlumbad@gmail.com>
2022-07-27 19:34:05 +0800

    added deletion of form section dynamically

--- server/app/models/form_section.rb
+++ server/app/models/form_section.rb
@@ -39,4 +39,32 @@ class FormSection < ApplicationRecord
 
         return response_data
     end
+
+    private
+        # DOCU: Method to delete a form_section dynamically
+        # Triggered by FormsController, FormSection
+        # Requires: params - fields_to_filter
+        # Returns: { status: true/false, result: form_details, error }
+        # Last updated at: July 26, 2022
+        # Owner: Adrian
+        def self.delete_form_section(params)
+            response_data = { :status => false, :result => {}, :error => nil }
+
+            begin
+                delete_form_record_query = ["
+                    DELETE FROM form_sections WHERE
+                "]
+
+                params[:fields_to_filter].each_with_index do |(field, value), index|
+                    delete_form_record_query[0] += " #{'AND' if index > 0} #{field} = ?"
+                    delete_form_record_query    << value
+                end
+
+                response_data.merge!(delete_record(delete_form_record_query).present? { :status => true } : { :error => "error in deletinng form_question, Please try again later" })
+            rescue Exception => ex
+                response_data[:error] = ex.message
+            end
+
+            return response_data
+        end
 end
