b4af5d1634ac51c03aa24b52f1eec15a8ff88d32
adriannicolai <anlumbad@gmail.com>
2022-07-27 19:27:30 +0800

    added deletion of form question dynamically

--- server/app/models/form_question.rb
+++ server/app/models/form_question.rb
@@ -51,4 +51,32 @@ class FormQuestion < ApplicationRecord
 
         return response_data
     end
+
+    private
+        # DOCU: Method to delete a question dynamically
+        # Triggered by FormsController, FormQuestion
+        # Requires: params - fields_to_filter
+        # Returns: { status: true/false, result: form_details,error }
+        # Last updated at: July 26, 2022
+        # Owner: Adrian
+        def self.delete_form_question(params)
+            response_data = { :status => false, :result => {}, :error => nil }
+
+            begin
+                delete_form_record_query = ["
+                    DELETE FROM form_questions WHERE
+                "]
+
+                params[:fields_to_filter].each_with_index do |(field, value), index|
+                    delete_form_record_query[0] += " #{'AND' if index > 0} #{field} = ?"
+                    delete_form_record_query    << value
+                end
+
+                response_data.merge!(delete_record(delete_form_record_query).present? { :status => true } : { :error => "error in deletinng form_question, Please try again later" })
+            rescue Exception => ex
+                response_data[:error] = ex.message
+            end
+
+            return response_data
+        end
 end
