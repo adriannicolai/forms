67bff17c7658a9dc13a3cabd161fbf4f3a7d0ee1
adriannicolai <anlumbad@gmail.com>
2022-07-27 21:02:38 +0800

    add getter and delete form section record

--- server/app/models/form_section.rb
+++ server/app/models/form_section.rb
@@ -40,10 +40,39 @@ class FormSection < ApplicationRecord
         return response_data
     end
 
+    # DOCU: Method to form_section dynamically
+    # Triggered by FormsController, FormSection
+    # Requires: params - fields_to_filter
+    # Optionals: params - fields_to_select
+    # Returns: { status: true/false, result: form_details, error }
+    # Last updated at: July 27, 2022
+    # Owner: Adrian
+    def self.get_form_section_record(params)
+        response_data = { :status => false, :result => {}, :error => nil }
+
+        begin
+            params[:fields_to_select] ||= "*"
+
+            get_form_section_query = ["
+                SELECT #{ActiveRecord::Base.sanitize_sql(params[:fields_to_select])} FROM form_sections WHERE
+            "]
+
+            params[:fields_to_filter].each_with_index do |(field, value), index|
+                get_form_section_query[0] += " #{ 'AND' if index > 0} #{field} = ?"
+                get_form_section_query    << value
+            end
+
+        rescue Exception => ex
+            response_data[:error] = ex.message
+        end
+
+        return response_data
+    end
+
     private
         # DOCU: Method to delete a form_section dynamically
         # Triggered by FormsController, FormSection
-        # Requires: params - fields_to_filter
+        # Requires: params - {fields_to_filter - form_id, form_section_id}
         # Returns: { status: true/false, result: form_details, error }
         # Last updated at: July 26, 2022
         # Owner: Adrian
@@ -51,13 +80,19 @@ class FormSection < ApplicationRecord
             response_data = { :status => false, :result => {}, :error => nil }
 
             begin
-                delete_form_record_query = ["
-                    DELETE FROM form_sections WHERE
-                "]
+                # TODO: Check if the form_section is exisitng
+                ActiveRecord::Base.transaction do
+                    # TODO: delete form_response
+                    # TODO: delete form qestion
+                    # TODO: delete form
+                    delete_form_record_query = ["
+                        DELETE FROM form_sections WHERE
+                    "]
 
-                params[:fields_to_filter].each_with_index do |(field, value), index|
-                    delete_form_record_query[0] += " #{'AND' if index > 0} #{field} = ?"
-                    delete_form_record_query    << value
+                    params[:fields_to_filter].each_with_index do |(field, value), index|
+                        delete_form_record_query[0] += " #{'AND' if index > 0} #{field} = ?"
+                        delete_form_record_query    << value
+                    end
                 end
 
                 response_data.merge!(delete_record(delete_form_record_query).present? { :status => true } : { :error => "error in deletinng form_question, Please try again later" })
