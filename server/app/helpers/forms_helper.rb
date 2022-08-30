8e32cbd9bad219cf6f01a733ca5e7f0fb4ab5471
adriannicolai <anlumbad@gmail.com>
2022-08-30 21:44:00 +0800

    add the helper for form question partial

--- server/app/helpers/forms_helper.rb
+++ server/app/helpers/forms_helper.rb
@@ -1,2 +1,26 @@
 module FormsHelper
+    # DOCU: Get the form question partial location by question type
+    # Triggered by: ViewForm
+    # Requires: question_type
+    # Returns: form_partial_location
+    # Last updated at: August 30, 2022
+    # Owner: Adrian
+    def get_form_question_partial_name(question_type)
+        form_partial_location = "forms/partials/form_question_partials/"
+
+        form_partial_location += case question_type.to_i
+        when QUESTION_SETTINGS[:question_type][:multiple_choice]
+            return "multiple_choice"
+        when QUESTION_SETTINGS[:question_type][:paragraph]
+            return "paragraph"
+        when QUESTION_SETTINGS[:question_type][:checkbox]
+            return "checkbox"
+        when QUESTION_SETTINGS[:question_type][:dropdown]
+            return "dropdown"
+        else
+            return "paragraph"
+        end
+
+        return form_partial_location
+    end
 end
