1619c4d13f2f9f0821564a06b72c5e264e05cb9d
adriannicolai <anlumbad@gmail.com>
2022-09-04 11:41:44 +0800

    add submission of the form

--- server/app/assets/javascripts/forms/view_form/view_form_be.js
+++ server/app/assets/javascripts/forms/view_form/view_form_be.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
     $(document)
+        .on("submit", "#create_question_form", submitCreateQuestionForm)       /* This function will handle the submission of create question via ajax */
 });
+
+/**
+* DOCU: This function is for submiting the add question form and adding the question to the DOM<br>
+* Triggered: .on("submit", "#create_question_form", submitCreateQuestionForm)<br>
+* Last Updated Date: September 4, 2022
+* @author Adrian
+*/
+function submitCreateQuestionForm(e){
+    e.preventDefault();
+
+    let create_question_form = $("#create_question_form");
+    
+    /* Sumbmit the create question form via ajax */
+    $.post(create_question_form.attr("action"), create_question_form.serialize(), (create_question_response) =>{
+        console.log(create_question_response);
+        if(create_question_response){
+            /* TODO: Append the question here */
+        }
+        else{
+            /* TODO: Add error message here */
+        }
+    });
+
+}
