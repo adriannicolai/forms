7489cfd02a009929430335a75fe6ab2306e9d796
adriannicolai <anlumbad@gmail.com>
2022-09-23 19:47:47 +0800

    updated the response

--- server/app/controllers/forms_controller.rb
+++ server/app/controllers/forms_controller.rb
@@ -105,14 +105,16 @@ class FormsController < ApplicationController
 			response_data[:status] = true
 
 			# Destructure the create_form_question
-			form_id, form_title = create_form_question[:result].values_at("form_question", "title")
+			form_id, form_title, form_section_id = create_form_question[:result].values_at("form_question", "title", "form_section_id")
 
 			# Render the form question
 			response_data[:result][:form_question] = render_to_string :partial => "/forms/partials/form_question_partials/paragraph", :locals => { :form_question => {
 				"form_question_id" => form_id,
-				"question_title"   => form_title, 
+				"question_title"   => form_title,
 			}}
 
+			# Append the form section id to the response
+			response_data[:result][:form_section_id] = form_section_id
 		rescue Exception => ex
 			response_data[:error] = ex.message
 		end
