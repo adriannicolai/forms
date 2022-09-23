7063aff3e9ef61dc2cd6663fe1616b7941b7e3b0
adriannicolai <anlumbad@gmail.com>
2022-09-23 19:23:57 +0800

    cleanup where creating a form question returns an html instead of data

--- server/app/controllers/forms_controller.rb
+++ server/app/controllers/forms_controller.rb
@@ -84,8 +84,8 @@ class FormsController < ApplicationController
 	# DOCU: This will handle the creation of new forms question
     # Triggered by: (POST) /forms/create_question
 	# Requires params - form_id, form_section_id
-    # Returns: { status: true/false, result: { question_details, question html }, error }
-    # Last updated at: September 4, 2022
+    # Returns: { status: true/false, result: { form_question }, error }
+    # Last updated at: September 23, 2022
     # Owner: Adrian
 	def create_question
 		response_data = { :status => false, :result => {}, :error => nil }
@@ -97,15 +97,24 @@ class FormsController < ApplicationController
 				"form_id"          => decrypt(params[:form_id]),
 				"form_section_id"  => params[:form_section_id]
 			})
-			
-			#TODO: Add the rendering of partial here for appending form question
 
-			# Merge the response data
-			response_data.merge!(create_form_question)
-
+			# Guard clause when creating form question is unsuccessful
 			raise create_form_question[:error] if !create_form_question[:status]
+
+			# Make the status of the response to true
+			response_data[:status] = true
+
+			# Destructure the create_form_question
+			form_id, form_title = create_form_question[:result].values_at("form_question", "title")
+
+			# Render the form question
+			response_data[:result][:form_question] = render_to_string :partial => "/forms/partials/form_question_partials/paragraph", :locals => { :form_question => {
+				"form_question_id" => form_id,
+				"question_title"   => form_title, 
+			}}
+
 		rescue Exception => ex
-			redirect_to_500
+			response_data[:error] = ex.message
 		end
 
 		render :json => response_data
