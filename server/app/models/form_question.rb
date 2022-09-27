da4a162c2c4115ecb49a6827dffe615e93639310
adriannicolai <anlumbad@gmail.com>
2022-09-27 22:32:50 +0800

    added transaction

--- server/app/models/form_question.rb
+++ server/app/models/form_question.rb
@@ -7,7 +7,7 @@ class FormQuestion < ApplicationRecord
 	# Requires: params - form_id, question_type_id, title
     # Optionals: params - choices
     # Returns: { status: true/false, result: form_details,error }
-    # Last updated at: August 31, 2022
+    # Last updated at: September 27, 2022
     # Owner: Adrian
     def self.create_form_question(params)
         response_data = { :status => false, :result => {}, :error => nil }
@@ -18,30 +18,32 @@ class FormQuestion < ApplicationRecord
             check_form_question_params = check_fields(["form_id", "form_section_id", "question_type_id", "title"], [], params)
 
             if check_form_question_params[:status]
-                # Create a new form question
-                create_form_question = insert_record(["
-                    INSERT INTO form_questions (form_id, form_section_id, question_type_id, title, is_required, created_at, updated_at)
-                    VALUES (?, ?, ?, ?, ?, NOW(), NOW())
-                ", check_form_question_params[:result][:form_id], check_form_question_params[:result][:form_section_id], check_form_question_params[:result][:question_type_id], check_form_question_params[:result][:title], BOOLEAN_FIELD[:no] ])
+                ActiveRecord::Base.transaction do
+                    # Create a new form question
+                    create_form_question = insert_record(["
+                        INSERT INTO form_questions (form_id, form_section_id, question_type_id, title, is_required, created_at, updated_at)
+                        VALUES (?, ?, ?, ?, ?, NOW(), NOW())
+                    ", check_form_question_params[:result][:form_id], check_form_question_params[:result][:form_section_id], check_form_question_params[:result][:question_type_id], check_form_question_params[:result][:title], BOOLEAN_FIELD[:no] ])
 
-                # Update the form section, append the new question to the question
-                if create_form_question.present?
-                    add_question_id_on_form_sections = update_record(["
-                        UPDATE form_sections SET
-                            form_question_ids = IF(
-                                JSON_CONTAINS(form_question_ids, ?, '$'), form_question_ids, JSON_ARRAY_APPEND(form_question_ids, '$', ?)
-                            )
-                        WHERE id = ?
-                    ", create_form_question, create_form_question, check_form_question_params[:result][:form_section_id]])
+                    # Update the form section, append the new question to the question
+                    if create_form_question.present?
+                        add_question_id_on_form_sections = update_record(["
+                            UPDATE form_sections SET
+                                form_question_ids = IF(
+                                    JSON_CONTAINS(form_question_ids, ?, '$'), form_question_ids, JSON_ARRAY_APPEND(form_question_ids, '$', ?)
+                                )
+                            WHERE id = ?
+                        ", create_form_question, create_form_question, check_form_question_params[:result][:form_section_id]])
 
-                    if add_question_id_on_form_sections.present?
-                        response_data[:status] = true
-                        response_data[:result] = self.get_form_question_record({:fields_to_filter => {:id => create_form_question}})[:result]
+                        if add_question_id_on_form_sections.present?
+                            response_data[:status] = true
+                            response_data[:result] = self.get_form_question_record({:fields_to_filter => {:id => create_form_question}})[:result]
+                        else
+                            raise "Error in adding form_question_ids, Please try again later"
+                        end
                     else
-                        raise "Error in adding form_question_ids, Please try again later"
+                        raise "Error in creating form question, Please try again later"
                     end
-                else
-                    response_data[:error] = "Error in creating form question, Please try again later"
                 end
             else
                 response_data.merge!(check_form_question_params)
@@ -64,7 +66,7 @@ class FormQuestion < ApplicationRecord
         response_data = { :status => false, :result => {}, :error => nil }
 
         begin
-            
+
         rescue Exception => ex
             response_data[:error] = ex.message
         end
