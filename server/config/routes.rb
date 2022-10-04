696a92fed1ee422697f43794203d06b3ebbaa709
adriannicolai <anlumbad@gmail.com>
2022-10-04 11:45:30 +0800

    move route to proper controller

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -17,11 +17,16 @@ Rails.application.routes.draw do
 		post "/get_form"   		=> "forms#get_form"
 		post "/create_form" 	=> "forms#create_form"
 		post "/delete_form" 	=> "forms#delete_form"
-		post "/create_question" => "forms#create_question"
+		post "/update_form"     => "forms#update_form"
 
 		# View form page
 		get  "/view" 	   		=> "forms#view_form"
 		post "/update_form"	 	=> "forms#update_form"
+	end
+
+	scope "form_questions" do
+		post "/create_form_question" => "form_questions#create_form_question"
+		post "update_form_question" => "form_questions#update_form_question"
 	end
 
 	# root logout
